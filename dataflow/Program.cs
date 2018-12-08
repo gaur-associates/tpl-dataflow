39a571a326f7a0e1b820bc90ef59587a92c1cd9f
yogesh gaur <yogi7.hills@gmail.com>
2018-12-08 13:15:25 -0600

    changes

--- dataflow/Program.cs
+++ dataflow/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+
 using System.Threading.Tasks.Dataflow;
 
 namespace dataflow
@@ -10,19 +11,21 @@ namespace dataflow
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var block = new ActionBlock<int>((timeout) =>
-            {
-                Thread.Sleep(timeout);
-                Console.WriteLine("in action block");
-            }, new ExecutionDataflowBlockOptions {
-                MaxDegreeOfParallelism = 2
-            });
+
+            var producerBlock = producer();
+
+            var consumer_1 = consumer("consumer 1");
+            var consumer_2 = consumer("consumer 2");
+
+            producerBlock.LinkTo(consumer_1);
+            producerBlock.LinkTo(consumer_2);
+
             var sw = new Stopwatch();
             sw.Start();
 
             for (int i = 0; i < 10; i++)
             {
-                if (block.Post(1000))
+                if (producerBlock.Post(i))
                 {
                     Console.WriteLine("Succesful post");
                 }
@@ -31,13 +34,47 @@ namespace dataflow
                     Console.WriteLine("Failed Post");
                 }
             }
-            block.Complete();
-            block.Completion.ContinueWith(p =>
+            producerBlock.Complete();
+            consumer_1.Completion.ContinueWith(p =>
             {
                 sw.Stop();
                 Console.WriteLine($"done - {sw.ElapsedMilliseconds}");
             });
+
+            
+            consumer_2.Completion.ContinueWith(p =>
+            {
+                sw.Stop();
+                Console.WriteLine($"done - {sw.ElapsedMilliseconds}");
+            });
+
+
             Console.ReadKey();
+        }
+
+        static TransformBlock<int, int> producer()
+        {
+            var block = new TransformBlock<int, int>(input => input * 2);
+
+            return block;
+        }
+
+        static ActionBlock<int> consumer(string name)
+        {
+            var block = new ActionBlock<int>(
+                (timeout) =>
+                {
+                    Thread.Sleep(timeout);
+                    Console.WriteLine($"in action block {name} - {timeout}");
+                },
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = 2,
+                    BoundedCapacity = 2
+
+                });
+
+            return block;
         }
     }
 }
